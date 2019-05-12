8db5b686e450b0902abb8fcda4c66993cb21ee2e
samlmcewan <samlmcewan@gmail.com>
2019-05-12 21:22:43 +0100

    Task 6 - On the details page, add an option to hide items that are marked as done.
    Tried to make a 'HideDone' checkbox similar to IsDone. Tried and failed to use Javascript to hide items marked as done when checkbox is checked.

--- Todo/Models/TodoLists/TodoListDetailViewmodel.cs
+++ Todo/Models/TodoLists/TodoListDetailViewmodel.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel;
 using Todo.Models.TodoItems;
 
 namespace Todo.Models.TodoLists
@@ -8,12 +9,21 @@ namespace Todo.Models.TodoLists
         public int TodoListId { get; }
         public string Title { get; }
         public ICollection<TodoItemSummaryViewmodel> Items { get; }
+        public bool HideDone { get; }
 
-        public TodoListDetailViewmodel(int todoListId, string title, ICollection<TodoItemSummaryViewmodel> items)
+        public TodoListDetailViewmodel(int todoListId, string title, ICollection<TodoItemSummaryViewmodel> items, bool hideDone)
         {
             Items = items;
             TodoListId = todoListId;
             Title = title;
+            HideDone = hideDone;
+        }
+
+        public TodoListDetailViewmodel(int todoListId, string title, List<TodoItemSummaryViewmodel> items)
+        {
+            TodoListId = todoListId;
+            Title = title;
+            Items = items;
         }
     }
 }
